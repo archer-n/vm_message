27f960c027c255eb999a33f6eb6064ca8a2b690d
archer <lazyarcher.wang@gmail.com>
2021-10-11 21:12:04 +0800

    Modify the log file output directory

---
+++ config/config.prod.js
@@ -0,0 +1,22 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  config.logger = {
+    dir: '/tmp/eggjs/' + appInfo.name,
+  };
+
+  return {
+    ...config,
+  };
+};
